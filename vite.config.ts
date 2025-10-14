3bd1bbbb1bfa5ea71df47faa309411fd8bbbe8d9
Jairo-Senna <142929627+Jairo-Senna@users.noreply.github.com>
2025-10-13 22:13:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // PROPRIEDADE CRUCIAL: Define o caminho base como o nome deste repositório
+      base: '/amigo-virtual/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
